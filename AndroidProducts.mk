581cf24db6b6e2cbae66d287617fc6100749058f
kwokdelee <121758372+kwokdelee@users.noreply.github.com>
2024-01-13 17:07:22 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,15 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_earth.mk
+    $(LOCAL_DIR)/omni_earth.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_earth-user \
-    twrp_earth-userdebug \
-    twrp_earth-eng
+    omni_earth-user \
+    omni_earth-userdebug \
+    omni_earth-eng
+
