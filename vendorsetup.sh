8a6b00114ef19fc47cdceeca7c4a00723a133c4a
kwokdelee <121758372+kwokdelee@users.noreply.github.com>
2024-01-13 17:12:57 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,10 @@
-add_lunch_combo twrp_earth-eng
-add_lunch_combo twrp_earth-user
-add_lunch_combo twrp_earth-userdebug
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_earth-user
+add_lunch_combo omni_earth-userdebug
+add_lunch_combo omni_earth-eng
